31eb15d9c5086e05847389497ae5f36adeb2df86
miyashd3 <3miya.10dai@gmail.com>
2025-10-06 23:19:21 +0900

    first commit

---
+++ 32/TestCode.java
@@ -0,0 +1,30 @@
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.Arrays;
+
+public class TestCode {
+
+    @SafeVarargs
+    static <T> T[] toArray(T... args) {
+        return args;
+    }
+
+    static <T> List<T> pickTwo(T a, T b, T c) {
+//        T[] result;
+        switch (ThreadLocalRandom.current().nextInt(3)) {
+            case 0: return List.of(a, b);
+            case 1: return List.of(a, c);
+            case 2: return List.of(b, c);
+            default: throw new AssertionError();
+        }
+        // 安全にコピーして T[] 型にする
+//        return Arrays.copyOf(result, result.length, (Class<T[]>) result.getClass());
+    }
+
+    public static void main(String[] args) {
+        List<String> result = pickTwo("A", "B", "C");
+        for (String s : result) {
+            System.out.println(s);
+        }
+    }
+}
